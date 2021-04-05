5d996ebdf7067f0083ab459bce65f556afc095d6
Abhishek Jani <abhishekjani62@gmail.com>
2021-04-05 09:07:42 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+bot = commands.Bot(command_prefix='!')
+@bot.event
+async def on_ready():
+    print("The bot is ready!")
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+@bot.command()
+async def whoareyou(ctx):
+    await ctx.send("I am Gaitonde ChatBot Created By Abhishek Jani for MLH EVENT")
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!')
+bot.run('ODI4NDYwMjc1MTYxMjM1NDU2.YGp5wQ.f-XwKQ-NKQNRM2OVQu1PnJ_VD7U')
